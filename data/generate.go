67b56ed781dee60421c681ce793b7897eeeca4cf
Indra Made <indra.made@frankieone.com>
2024-05-15 16:34:58 +0700

    added random generator function

--- data/generate.go
+++ data/generate.go
@@ -1,5 +1,10 @@
 package data
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func Generate(dataType string) string {
 	switch dataType {
 	case TYPE_NAME:
@@ -15,18 +20,31 @@ func Generate(dataType string) string {
 	return ""
 }
 
+// generateName generates a random name from data
 func generateName() string {
-	return ""
+	nameLen := len(Name)
+
+	return Name[rand.Intn(nameLen)]
 }
 
+// generateDate generates a random date with format YYYY-MM-DD
 func generateDate() string {
-	return ""
+	year := rand.Intn(100) + 1950
+	month := rand.Intn(12) + 1
+	day := rand.Intn(28) + 1
+
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
+// generateAddress generates a random address from combination of street, random number, and city
 func generateAddress() string {
-	return ""
+	streetLen := len(Address["street"])
+	cityLen := len(Address["city"])
+
+	return fmt.Sprintf("%s No. %d, %s", Address["street"][rand.Intn(streetLen)], rand.Intn(100), Address["city"][rand.Intn(cityLen)])
 }
 
+// generatePhone generates a random phone number with 12 digits limit
 func generatePhone() string {
-	return ""
+	return fmt.Sprintf("08%d", rand.Intn(10000000000))
 }
